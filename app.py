7167bfa92c56b55d4c04ba8efc9cb407a0df6dba
sergio <tamkovich1072@gmail.com>
2022-12-20 16:42:29 +0300

    "Create DB"

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import copy
+
 import fastapi
 import database
 import pydantic_models
 import config
+from fastapi import Request  # позволяет нам перехватывать запрос и получать по нему всю информацию
 
 api = fastapi.FastAPI()
 
@@ -54,7 +57,7 @@ fake_database = {'users': [
         "id": 3,  # у третьего
         "name": "Vladimir",  # юзера
         "nick": "Vova777",  # мы специально сделаем
-        "balance": "25000"  # нестандартный тип данных в его балансе
+        "balance": 200.1  # нестандартный тип данных в его балансе
     }
 ], }
 
@@ -84,6 +87,7 @@ def get_total_balance():
 def get_users(skip: int = 0, limit: int = 10):
     return fake_database['users'][skip: skip + limit]
 
+
 # Когда мы задаем аргументы в функции обработки запроса мы можем им дать значения по умолчанию,
 # причем делая это с применением тайп-хинтов. Если один из параметров совсем необязателен,
 # то можно сделать так:
@@ -96,3 +100,51 @@ def read_user(user_id: str, query: str | None = None):
     if query:
         return {"user_id": user_id, "query": query}
     return {"user_id": user_id}
+
+
+@api.get('/')  # метод для обработки get запросов
+@api.post('/')  # метод для обработки post запросов
+@api.put('/')  # метод для обработки put запросов
+@api.delete('/')  # метод для обработки delete запросов
+def index(request: Request):  # тут request - будет объектом в котором хранится вся информация о запросе
+    return {"Request": [request.method,  # тут наш API вернет клиенту метод, с которым этот запрос был совершен
+                        request.headers]}  # а тут в ответ вернутся все хедеры клиентского запроса
+
+
+@api.post('/user/create')
+def index(user: pydantic_models.User):
+    """
+    Когда в пути нет никаких параметров
+    и не используются никакие переменные,
+    то fastapi, понимая, что у нас есть аргумент, который
+    надо заполнить, начинает искать его в теле запроса,
+    в данном случае он берет информацию, которую мы ему отправляем
+    в теле запроса и сверяет её с моделью pydantic, если всё хорошо,
+    то в аргумент user будет загружен наш объект, который мы отправим
+    на сервер.
+    """
+    fake_database['users'].append(user)
+    return {'User Created!': user}
+
+
+@api.put('/user/{user_id}')
+def update_user(user_id: int,
+                user: pydantic_models.User = fastapi.Body()):  # используя fastapi.Body() мы явно указываем, что отправляем информацию в теле запроса
+    for index, u in enumerate(fake_database[
+                                  'users']):  # так как в нашей бд юзеры хранятся в списке, нам нужно найти их индексы внутри этого списка
+        if u['id'] == user_id:
+            fake_database['users'][index] = user  # обновляем юзера в бд по соответствующему ему индексу из списка users
+            return user
+
+
+@api.delete('/user/{user_id}')
+def delete_user(
+        user_id: int = fastapi.Path()):  # используя fastapi.Path() мы явно указываем, что переменную нужно брать из пути
+    for index, u in enumerate(fake_database[
+                                  'users']):  # так как в нашей бд юзеры хранятся в списке, нам нужно найти их индексы внутри этого списка
+        if u['id'] == user_id:
+            old_db = copy.deepcopy(
+                fake_database)  # делаем полную копию объекта в переменную old_db, чтобы было с чем сравнить
+            del fake_database['users'][index]  # удаляем юзера из бд
+            return {'old_db': old_db,
+                    'new_db': fake_database}
--- database/db.py
+++ database/db.py
@@ -1,1 +1,31 @@
-import pony
+from pony.orm import *
+
+db = Database()
+
+
+# создаём модели (Сущностей (Entity на английском)
+
+class User(db.Entity):
+    """Пользователь"""
+    user_id = Required(str)
+    nick = Required(str)
+    age = Required(int)
+    wallets = Set('Wallet')
+
+
+class Wallet(db.Entity):
+    """Кошелёк"""
+    address = Required(str)
+    private_key = Required(str)
+    owner = Required(User)
+
+# Database.bind(). Он используется для привязки объявленных сущностей к определенной базе данных
+# filename=':memory:' - для хранения бд в памяти
+try:
+    db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+except Exception as Ex:
+    print(Ex)
+# db.generate_mapping(create_tables=True)
+# Параметр create_tables=True указывает, что если таблицы еще не существуют,
+# то они будут созданы с помощью команды CREATE TABLE
+
